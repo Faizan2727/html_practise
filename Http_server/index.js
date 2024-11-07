3882351ce3be8cc5b59412aca933bb25d27b9cdb
Faizan2727 <faizanshaikhbmw2727@gmail.com>
2024-11-07 21:35:38 +0530

    chnages in index.js file

--- Http_server/index.js
+++ Http_server/index.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const bodyParser = require("body-parser")
 const port = 4000
 const app = express()
 
-app.get('/', function(req, res){
-    res.send('Hello World')
+app.use(bodyParser.json({}))
+app.post('/', function(req, res){
+    console.log(req.body)
+    //console.log(req.headers)
+    res.send("Hello 2")
 })
 
+app.post('/con', function(req, res){
+    const message = req.body.message
+    console.log(message)
+    //console.log(req.headers)
+    res.json({
+        output: "2 + 2 = 4"
+    })
+})
 app.listen(port)
