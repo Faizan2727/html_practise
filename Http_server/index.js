3f9d690584d4214870e45b6a5eaeb81a42ca72d3
Faizan2727 <faizanshaikhbmw2727@gmail.com>
2024-11-08 15:23:10 +0530

    chnages in  file

--- Http_server/index.js
+++ Http_server/index.js
@@ -1,21 +1,53 @@
+// const express = require('express')
+// const bodyParser = require("body-parser")
+// const port = 4000
+// const app = express()
+// console.log(app)//see which function express support 
+
+// app.use(bodyParser.json({}))
+// app.post('/', function(req, res){
+//     console.log(req.body)
+//     //console.log(req.headers)
+//     res.send("Hello 2")
+// })
+
+// app.post('/con', function(req, res){
+//     const message = req.body.message
+//     console.log(message)
+//     //console.log(req.headers)
+//     res.json({
+//         output: "2 + 2 = 4"
+//     })
+// })
+
+// app.post('/q', function(req, res){
+//     const message = req.query.message
+//     console.log(message)
+//     //console.log(req.headers)
+//     res.json({
+//         output: "2 + 2 = 4"
+//     })
+// })
+// app.listen(port)
+
 const express = require('express')
-const bodyParser = require("body-parser")
-const port = 4000
 const app = express()
+const port = 3000
 
-app.use(bodyParser.json({}))
-app.post('/', function(req, res){
-    console.log(req.body)
-    //console.log(req.headers)
-    res.send("Hello 2")
+function calculateSum(n){
+    let ans = 0;
+    for(let i = 1; i<n; i++){
+        ans = ans + i;
+    }
+    return ans;
+}
+
+app.get('/con', (req, res) => {
+    const n = req.query.n;
+    const ans = calculateSum(n)
+    res.send("your answer is " + ans.toString());
 })
 
-app.post('/con', function(req, res){
-    const message = req.body.message
-    console.log(message)
-    //console.log(req.headers)
-    res.json({
-        output: "2 + 2 = 4"
-    })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
-app.listen(port)
